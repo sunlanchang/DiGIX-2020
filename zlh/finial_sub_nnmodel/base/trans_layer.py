93567e4d86bf153bed49a144fe34a5c66aeccea2
阿郑zzz <zlh1992@126.com>
2020-10-01 18:55:44 +0800

    stage1=A榜 stage2=B榜 stage2的5个步骤都运行下，然后运行stage2的两个model即可得到参与融合的两种模型 stage1中的cross_emb与stage2的get_cross_emb做法一致，后续在b榜中表现不佳，后做了简化处理得出model_cross_emb 提交的代码中也包括了stage1的sota模型，当时单模型90%数据单折可达0.8057+ 模型融合主要为数个10折（不同采样比，有无-1缺失值填充,两种模型）参与 融合方式包括了gmeans与加权融合

---
+++ zlh/finial_sub_nnmodel/base/trans_layer.py
@@ -0,0 +1,279 @@
+from __future__ import print_function
+
+import os
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Layer
+
+
+class Embedding(Layer):
+
+    def __init__(self, vocab_size, model_dim, **kwargs):
+        self._vocab_size = vocab_size
+        self._model_dim = model_dim
+        super(Embedding, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.embeddings = self.add_weight(
+            shape=(self._vocab_size, self._model_dim),
+            initializer='glorot_uniform',
+            name="embeddings")
+        super(Embedding, self).build(input_shape)
+
+    def call(self, inputs):
+        if K.dtype(inputs) != 'int32':
+            inputs = K.cast(inputs, 'int32')
+        embeddings = K.gather(self.embeddings, inputs)
+        embeddings *= self._model_dim ** 0.5  # Scale
+        return embeddings
+
+    def compute_output_shape(self, input_shape):
+
+        return input_shape + (self._model_dim,)
+
+
+class PositionEncoding(Layer):
+
+    def __init__(self, model_dim, **kwargs):
+        self._model_dim = model_dim
+        super(PositionEncoding, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        seq_length = inputs.shape[1]
+        position_encodings = np.zeros((seq_length, self._model_dim))
+        for pos in range(seq_length):
+            for i in range(self._model_dim):
+                position_encodings[pos, i] = pos / \
+                    np.power(10000, (i-i % 2) / self._model_dim)
+        position_encodings[:, 0::2] = np.sin(position_encodings[:, 0::2])  # 2i
+        position_encodings[:, 1::2] = np.cos(
+            position_encodings[:, 1::2])  # 2i+1
+        position_encodings = K.cast(position_encodings, 'float32')
+        return position_encodings
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class ScaledDotProductAttention(Layer):
+
+    def __init__(self, masking=True, future=False, dropout_rate=0., **kwargs):
+        self._masking = masking
+        self._future = future
+        self._dropout_rate = dropout_rate
+        self._masking_num = -2**32+1
+        super(ScaledDotProductAttention, self).__init__(**kwargs)
+
+    def mask(self, inputs, masks):
+        masks = K.cast(masks, 'float32')
+        masks = K.tile(masks, [K.shape(inputs)[0] // K.shape(masks)[0], 1])
+        masks = K.expand_dims(masks, 1)
+        outputs = inputs + masks * self._masking_num
+        return outputs
+
+    def future_mask(self, inputs):
+        diag_vals = tf.ones_like(inputs[0, :, :])
+        tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()
+        future_masks = tf.tile(tf.expand_dims(tril, 0), [
+                               tf.shape(inputs)[0], 1, 1])
+        paddings = tf.ones_like(future_masks) * self._masking_num
+        outputs = tf.where(tf.equal(future_masks, 0), paddings, inputs)
+        return outputs
+
+    def call(self, inputs):
+        if self._masking:
+            assert len(
+                inputs) == 4, "inputs should be set [queries, keys, values, masks]."
+            queries, keys, values, masks = inputs
+        else:
+            assert len(
+                inputs) == 3, "inputs should be set [queries, keys, values]."
+            queries, keys, values = inputs
+
+        if K.dtype(queries) != 'float32':
+            queries = K.cast(queries, 'float32')
+        if K.dtype(keys) != 'float32':
+            keys = K.cast(keys, 'float32')
+        if K.dtype(values) != 'float32':
+            values = K.cast(values, 'float32')
+
+        matmul = K.batch_dot(queries, tf.transpose(keys, [0, 2, 1]))  # MatMul
+        scaled_matmul = matmul / int(queries.shape[-1]) ** 0.5  # Scale
+        if self._masking:
+            scaled_matmul = self.mask(scaled_matmul, masks)  # Mask(opt.)
+
+        if self._future:
+            scaled_matmul = self.future_mask(scaled_matmul)
+
+        softmax_out = K.softmax(scaled_matmul)  # SoftMax
+        # Dropout
+        out = K.dropout(softmax_out, self._dropout_rate)
+
+        outputs = K.batch_dot(out, values)
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class MultiHeadAttention(Layer):
+
+    def __init__(self, n_heads, head_dim, dropout_rate=.1, masking=True, future=False, trainable=True, **kwargs):
+        self._n_heads = n_heads
+        self._head_dim = head_dim
+        self._dropout_rate = dropout_rate
+        self._masking = masking
+        self._future = future
+        self._trainable = trainable
+        super(MultiHeadAttention, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self._weights_queries = self.add_weight(
+            shape=(input_shape[0][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_queries')
+        self._weights_keys = self.add_weight(
+            shape=(input_shape[1][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_keys')
+        self._weights_values = self.add_weight(
+            shape=(input_shape[2][-1], self._n_heads * self._head_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name='weights_values')
+        super(MultiHeadAttention, self).build(input_shape)
+
+    def call(self, inputs):
+        if self._masking:
+            assert len(
+                inputs) == 4, "inputs should be set [queries, keys, values, masks]."
+            # (bs, 100, 256)
+            queries, keys, values, masks = inputs
+        else:
+            assert len(
+                inputs) == 3, "inputs should be set [queries, keys, values]."
+            queries, keys, values = inputs
+        # (bs, 100, 256)*(256, 512) ==> (bs, 100, 512)
+        queries_linear = K.dot(queries, self._weights_queries)
+        keys_linear = K.dot(keys, self._weights_keys)
+        values_linear = K.dot(values, self._weights_values)
+
+        queries_multi_heads = tf.concat(
+            tf.split(queries_linear, self._n_heads, axis=2), axis=0)
+        keys_multi_heads = tf.concat(
+            tf.split(keys_linear, self._n_heads, axis=2), axis=0)
+        values_multi_heads = tf.concat(
+            tf.split(values_linear, self._n_heads, axis=2), axis=0)
+
+        if self._masking:
+            att_inputs = [queries_multi_heads,
+                          keys_multi_heads, values_multi_heads, masks]
+        else:
+            att_inputs = [queries_multi_heads,
+                          keys_multi_heads, values_multi_heads]
+
+        attention = ScaledDotProductAttention(
+            masking=self._masking, future=self._future, dropout_rate=self._dropout_rate)
+        # att_out: (bs, 100, 64)
+        att_out = attention(att_inputs)
+
+        outputs = tf.concat(tf.split(att_out, self._n_heads, axis=0), axis=2)
+        # print('**********************',
+        #       len(tf.split(att_out, self._n_heads, axis=0)))
+        # print('**********************',
+        #       [e.shape for e in tf.split(att_out, self._n_heads, axis=0)])
+        return outputs
+        # return queries_linear
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class PositionWiseFeedForward(Layer):
+
+    def __init__(self, model_dim, inner_dim, trainable=True, **kwargs):
+        self._model_dim = model_dim
+        self._inner_dim = inner_dim
+        self._trainable = trainable
+        super(PositionWiseFeedForward, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.weights_inner = self.add_weight(
+            shape=(input_shape[-1], self._inner_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name="weights_inner")
+        self.weights_out = self.add_weight(
+            shape=(self._inner_dim, self._model_dim),
+            initializer='glorot_uniform',
+            trainable=self._trainable,
+            name="weights_out")
+        self.bais_inner = self.add_weight(
+            shape=(self._inner_dim,),
+            initializer='uniform',
+            trainable=self._trainable,
+            name="bais_inner")
+        self.bais_out = self.add_weight(
+            shape=(self._model_dim,),
+            initializer='uniform',
+            trainable=self._trainable,
+            name="bais_out")
+        super(PositionWiseFeedForward, self).build(input_shape)
+
+    def call(self, inputs):
+        if K.dtype(inputs) != 'float32':
+            inputs = K.cast(inputs, 'float32')
+        inner_out = K.relu(K.dot(inputs, self.weights_inner) + self.bais_inner)
+        outputs = K.dot(inner_out, self.weights_out) + self.bais_out
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return self._model_dim
+
+
+class LayerNormalization(Layer):
+
+    def __init__(self, epsilon=1e-8, **kwargs):
+        self._epsilon = epsilon
+        super(LayerNormalization, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.beta = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer='zero',
+            name='beta')
+        self.gamma = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer='one',
+            name='gamma')
+        super(LayerNormalization, self).build(input_shape)
+
+    def call(self, inputs):
+        mean, variance = tf.nn.moments(inputs, [-1], keepdims=True)
+        normalized = (inputs - mean) / ((variance + self._epsilon) ** 0.5)
+        outputs = self.gamma * normalized + self.beta
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
+class Add(Layer):
+
+    def __init__(self, **kwargs):
+        super(Add, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        input_a, input_b = inputs
+        return input_a + input_b
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[0]
+
+
+if __name__ == "__main__":
+    pass
